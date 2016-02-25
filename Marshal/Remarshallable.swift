c4710524a6fc12bd5281318e52408a83dca931fd
Jason Larsen <jason.larsen@gmail.com>
2016-02-25 09:57:48 -0700

    add Remarshallable
    refs #8

---
+++ Marshal/Remarshallable.swift
@@ -0,0 +1,19 @@
+//
+//  M A R S H A L
+//
+//       ()
+//       /\
+//  ()--'  '--()
+//    `.    .'
+//     / .. \
+//    ()'  '()
+//
+//
+
+
+import Foundation
+
+
+public protocol Remarshallable {
+    func remarshall(object: Object)
+}
