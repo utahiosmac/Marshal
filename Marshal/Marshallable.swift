7c7b1bfeafa0e8a299ae1c6f0167e2600217a61d
Jason Larsen <jason.larsen@gmail.com>
2016-02-25 11:03:27 -0700

    everything is backwards

--- Marshal/Marshallable.swift
+++ Marshal/Marshallable.swift
@@ -12,22 +12,6 @@
 
 import Foundation
 
-
-public protocol Marshallable : ValueType {
-    typealias ConvertibleType = Self
-    init(object: Object) throws
-}
-
-extension Marshallable {
-    
-    public static func value(object: Any) throws -> ConvertibleType {
-        guard let convertedObject = object as? Object else {
-            throw Error.TypeMismatch(expected: Object.self, actual: object.dynamicType)
-        }
-        guard let value = try self.init(object: convertedObject) as? ConvertibleType else {
-            throw Error.TypeMismatch(expected: ConvertibleType.self, actual: object.dynamicType)
-        }
-        return value
-    }
-    
+public protocol Marshallable {
+    func marshall() -> Object
 }
--- Marshal/Unmarshallable.swift
+++ Marshal/Unmarshallable.swift
@@ -13,6 +13,21 @@
 
 import Foundation
 
-public protocol Unmarshallable {
-    func unmarshall() -> Object
+public protocol Unmarshallable : ValueType {
+    typealias ConvertibleType = Self
+    init(object: Object) throws
+}
+
+extension Unmarshallable {
+    
+    public static func value(object: Any) throws -> ConvertibleType {
+        guard let convertedObject = object as? Object else {
+            throw Error.TypeMismatch(expected: Object.self, actual: object.dynamicType)
+        }
+        guard let value = try self.init(object: convertedObject) as? ConvertibleType else {
+            throw Error.TypeMismatch(expected: ConvertibleType.self, actual: object.dynamicType)
+        }
+        return value
+    }
+    
 }
