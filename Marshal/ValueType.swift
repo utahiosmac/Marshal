d21bb8926898490b4ff3b283b6af6a7dae111d5d
Brian Mullen <programmer.mullen@gmail.com>
2016-02-25 17:37:40 -0700

    replace NSNumber with Int for Int64 extraction
    This favors using Swift's builtin Int support instead of bridging back and forth with NSNumber.

--- Marshal/ValueType.swift
+++ Marshal/ValueType.swift
@@ -43,8 +43,9 @@ extension Bool: ValueType {}
 
 extension Int64: ValueType {
     public static func value(object: Any) throws -> Int64 {
-        guard let value = object as? NSNumber else { throw Error.TypeMismatch(expected: NSNumber.self, actual: object.dynamicType) }
-        return value.longLongValue
+        guard let value = object as? Int else { throw Error.TypeMismatch(expected: _Value.self, actual: object.dynamicType) }
+        
+        return Int64( value )
     }
 }
 
