d15973b71f4d6572676f5bf2f248fc4a06450615
Bart Whiteley <bwhiteley@gmail.com>
2016-05-29 22:09:18 -0600

    use `valueForKeyPath` on NSDictionary for better performance.

--- Sources/MarshalDictionary.swift
+++ Sources/MarshalDictionary.swift
@@ -30,6 +30,35 @@ extension Dictionary: MarshaledObject {
 }
 
 extension NSDictionary: MarshaledObject {
+    public func anyForKey(key: KeyType) throws -> Any {
+        let value:Any
+        if key.dynamicType.keyTypeSeparator == "." {
+            guard let v = self.valueForKeyPath(key.stringValue) else {
+                throw Error.KeyNotFound(key: key)
+            }
+            value = v
+        }
+        else {
+            let pathComponents = key.split()
+            var accumulator: Any = self
+
+            for component in pathComponents {
+                if let componentData = accumulator as? MarshaledObject, v = componentData[component] {
+                    accumulator = v
+                    continue
+                }
+                throw Error.KeyNotFound(key: key.stringValue)
+            }
+            value = accumulator
+        }
+
+        if let _ = value as? NSNull {
+            throw Error.NullValue(key: key)
+        }
+
+        return value
+    }
+
     public subscript(key: KeyType) -> Any? {
         guard let aKey = key as? Key else { return nil }
         
