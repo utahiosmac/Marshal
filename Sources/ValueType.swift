dc0288ce7255880640041a909cf7f2ff6d1d52d2
Bart Whiteley <@bwhiteley>
2018-04-14 08:39:14 -0600

    add compactMap for compatibility with Swift 4.0

--- Sources/ValueType.swift
+++ Sources/ValueType.swift
@@ -206,3 +206,14 @@ extension Character: ValueType {
         return Character(value)
     }
 }
+
+#if swift(>=4.1)
+#else
+    extension Collection {
+        func compactMap<ElementOfResult>(
+            _ transform: (Element) throws -> ElementOfResult?
+            ) rethrows -> [ElementOfResult] {
+            return try flatMap(transform)
+        }
+    }
+#endif
